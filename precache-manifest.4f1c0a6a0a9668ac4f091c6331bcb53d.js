4559d3a11e52242ce5024e0e318e032c750cf71f
Jeremy Downes <jeremydownes@hotmail.com>
2020-10-09 08:35:56 -0400

    build

---
+++ precache-manifest.4f1c0a6a0a9668ac4f091c6331bcb53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0262e9003cafc2814e0198a443c6e351",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d63a536fa2d95aed0a01",
+    "url": "/static/css/main.2a959ab0.chunk.css"
+  },
+  {
+    "revision": "e48cce003ed2f126ba81",
+    "url": "/static/js/2.240ef753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.240ef753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63a536fa2d95aed0a01",
+    "url": "/static/js/main.a10ecd0d.chunk.js"
+  },
+  {
+    "revision": "3f8f99b36183861cb353",
+    "url": "/static/js/runtime-main.9bd5d0ab.js"
+  }
+]);
